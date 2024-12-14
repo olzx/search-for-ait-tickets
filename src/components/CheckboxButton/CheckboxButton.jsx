1e043c5baf718aa0098610483fc7f199a0fdd7d0
Олежка <ol2az@yandex.ru>
2024-12-15 01:24:51 +0700

    added the ability to switch the checkboxButton into two states on/off

--- src/components/CheckboxButton/CheckboxButton.jsx
+++ src/components/CheckboxButton/CheckboxButton.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
 import "./CheckboxButton.css"
 
-export default function CheckboxButton({ children }) {
+export default function CheckboxButton({ onClick, children }) {
+    const [isActive, setisActive] = useState(false)
+
+    const isCheckmark = isActive && <span>&#x2713;</span>
+
+    function checboxClick() {
+        setisActive((prev) => !prev)
+        onClick && onClick(!isActive)
+    }
+
     return (
-        <div className="checkbox-button">
+        <div className="checkbox-button" onClick={checboxClick}>
             <div className="checkbox-button__square">
-                <div className="checkbox-button__checkmark">&#x2713;</div>
+                <div className="checkbox-button__checkmark">{isCheckmark}</div>
             </div>
             <div className="checkbox-button__label">{children}</div>
         </div>
