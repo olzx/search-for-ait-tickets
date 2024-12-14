02af934fca771618527c0cdf46a807d5c24bba00
Олежка <ol2az@yandex.ru>
2024-12-15 05:45:39 +0700

    added image in top page site

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
 import "./app.scss"
+import airPlane from "./assets/airPlane.jpg"
 import Sorting from "./components/Sorting/Sorting"
 import TicketCard from "./components/TicketCard/TicketCard"
 import Transfer from "./components/Transfer/Transfer"
 
 export default function App() {
     return (
-        <div className="content">
-            <div className="content__leftPanel">
-                <Transfer
-                    onClick={(tabName) =>
-                        console.log(
-                            "В блоке Transfer выбрана вкладка: " + tabName
-                        )
-                    }
-                />
+        <>
+            <div className="image">
+                <img className="image__content" src={airPlane} alt="" />
             </div>
-            <div className="content__rightPanel">
-                <Sorting
-                    onClick={(tabName) =>
-                        console.log(
-                            "В блоке Sorting выбрана вкладка: " + tabName
-                        )
-                    }
-                />
-                <TicketCard></TicketCard>
-                <TicketCard></TicketCard>
-                <TicketCard></TicketCard>
+            <div className="content">
+                <div className="content__leftPanel">
+                    <Transfer
+                        onClick={(tabName) =>
+                            console.log(
+                                "В блоке Transfer выбрана вкладка: " + tabName
+                            )
+                        }
+                    />
+                </div>
+                <div className="content__rightPanel">
+                    <Sorting
+                        onClick={(tabName) =>
+                            console.log(
+                                "В блоке Sorting выбрана вкладка: " + tabName
+                            )
+                        }
+                    />
+                    <TicketCard></TicketCard>
+                    <TicketCard></TicketCard>
+                    <TicketCard></TicketCard>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
