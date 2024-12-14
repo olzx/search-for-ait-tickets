346749aec221729e584464ca9736dc5e2b4403e7
Олежка <ol2az@yandex.ru>
2024-12-15 02:27:18 +0700

    the logic of the CheckboxButton and the Sorting block has been changed. Now they work like tabs

--- src/components/CheckboxButton/CheckboxButton.jsx
+++ src/components/CheckboxButton/CheckboxButton.jsx
@@ -1,18 +1,11 @@
 import { useState } from "react"
 import "./CheckboxButton.css"
 
-export default function CheckboxButton({ onClick, children }) {
-    const [isActive, setisActive] = useState(false)
-
+export default function CheckboxButton({ children, isActive, ...props }) {
     const isCheckmark = isActive && <span>&#x2713;</span>
 
-    function checboxClick() {
-        setisActive((prev) => !prev)
-        onClick && onClick(!isActive)
-    }
-
     return (
-        <div className="checkbox-button" onClick={checboxClick}>
+        <div className="checkbox-button" {...props}>
             <div className="checkbox-button__square">
                 <div className="checkbox-button__checkmark">{isCheckmark}</div>
             </div>
--- src/components/Transfer/Transfer.jsx
+++ src/components/Transfer/Transfer.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react"
 import CheckboxButton from "../CheckboxButton/CheckboxButton"
 import "./Transfer.css"
 
-export default function Transfer() {
+export default function Transfer({ onClick }) {
+    const [tabName, setTabName] = useState("all")
+
+    function toggleTabName(name) {
+        setTabName(name)
+        onClick && onClick(tabName)
+    }
+
     return (
         <div className="transfer">
             <p className="transfer__title">Количество пересадок</p>
-            <CheckboxButton>Все</CheckboxButton>
-            <CheckboxButton>Без пересадки</CheckboxButton>
-            <CheckboxButton>1 пересадка</CheckboxButton>
+            <CheckboxButton
+                onClick={() => toggleTabName("all")}
+                isActive={tabName == "all"}
+            >
+                Все
+            </CheckboxButton>
+            <CheckboxButton
+                onClick={() => toggleTabName("without")}
+                isActive={tabName == "without"}
+            >
+                Без пересадки
+            </CheckboxButton>
+            <CheckboxButton
+                onClick={() => toggleTabName("one")}
+                isActive={tabName == "one"}
+            >
+                1 пересадка
+            </CheckboxButton>
         </div>
     )
 }
