1fcc2c829e29b6d75071b72d42144b5aeecc10b4
Олежка <ol2az@yandex.ru>
2024-12-15 05:32:26 +0700

    added styles to center the entire page

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,28 @@ import Transfer from "./components/Transfer/Transfer"
 
 export default function App() {
     return (
-        <>
-            <Transfer
-                onClick={(tabName) =>
-                    console.log("В блоке Transfer выбрана вкладка: " + tabName)
-                }
-            />
-            <Sorting
-                onClick={(tabName) =>
-                    console.log("В блоке Sorting выбрана вкладка: " + tabName)
-                }
-            />
-            <TicketCard></TicketCard>
-            <TicketCard></TicketCard>
-            <TicketCard></TicketCard>
-        </>
+        <div className="content">
+            <div className="content__leftPanel">
+                <Transfer
+                    onClick={(tabName) =>
+                        console.log(
+                            "В блоке Transfer выбрана вкладка: " + tabName
+                        )
+                    }
+                />
+            </div>
+            <div className="content__rightPanel">
+                <Sorting
+                    onClick={(tabName) =>
+                        console.log(
+                            "В блоке Sorting выбрана вкладка: " + tabName
+                        )
+                    }
+                />
+                <TicketCard></TicketCard>
+                <TicketCard></TicketCard>
+                <TicketCard></TicketCard>
+            </div>
+        </div>
     )
 }
