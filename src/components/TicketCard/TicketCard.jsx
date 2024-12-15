be69ccbd69f0f554f195136869d4a177d935abc7
Олежка <ol2az@yandex.ru>
2024-12-15 07:38:35 +0700

    added dynamic display of data in a component

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -1,42 +1,62 @@
 import "./TicketCard.scss"
 
-export default function TicketCard() {
+export default function TicketCard({ ticketData }) {
     return (
         <div className="ticketCard">
             <div className="ticketCard__leftPanel">
-                <div className="ticketCard__price">10 000 Руб</div>
+                <div className="ticketCard__price">{ticketData.price}</div>
                 <div className="selectButton">Выбрать билет</div>
             </div>
             <div className="ticketCard__rightPanel">
                 <div className="ticketCard__upPanel">
                     <div className="airline">
-                        <div className="airline__name">Аэрофлот</div>
+                        <div className="airline__name">
+                            {ticketData.airline}
+                        </div>
                     </div>
                 </div>
                 <div className="ticketCard__middlePanel">
                     <div className="flightInfo">
                         <div className="flightInfo__left">
-                            <div className="flightInfo__time">05:10</div>
-                            <div className="flightInfo__city">Новосибирск</div>
-                            <div className="flightInfo__date">15 дек</div>
+                            <div className="flightInfo__time">
+                                {ticketData.flightInfo.departureTime[0]}
+                            </div>
+                            <div className="flightInfo__city">
+                                {ticketData.flightInfo.origin}
+                            </div>
+                            <div className="flightInfo__date">
+                                {ticketData.flightInfo.departureTime[1]}
+                            </div>
                         </div>
                         <div className="flightInfo__middle">
                             <div className="flightInfo__duration">
-                                В пути: 1д 12ч 25м
+                                В пути: {ticketData.flightInfo.duration}
                             </div>
                         </div>
                         <div className="flightInfo__right">
-                            <div className="flightInfo__time">17:35</div>
-                            <div className="flightInfo__city">Пхукет</div>
-                            <div className="flightInfo__date">16 дек</div>
+                            <div className="flightInfo__time">
+                                {ticketData.flightInfo.arrivalTime[0]}
+                            </div>
+                            <div className="flightInfo__city">
+                                {ticketData.flightInfo.destination}
+                            </div>
+                            <div className="flightInfo__date">
+                                {ticketData.flightInfo.arrivalTime[1]}
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="ticketCard__downPanel">
                     <div className="transfers">
-                        <div className="transfers__amount">Пересадки - 5</div>
+                        <div className="transfers__amount">
+                            Пересадки - {ticketData.transfers}
+                        </div>
                         <div className="transfers__locations">
-                            Париж, Нью-Йорк, Мехико, Сантьяго, Богото
+                            {ticketData.transfers ? (
+                                ticketData.transferLocations.join(", ")
+                            ) : (
+                                <span></span>
+                            )}
                         </div>
                     </div>
                 </div>
