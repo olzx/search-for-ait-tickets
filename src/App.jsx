d11dec7fb2f8ef86ddd95698955c144dcd69e522
Олежка <ol2az@yandex.ru>
2024-12-15 09:27:56 +0700

    added filter and sort functions. added preloader in start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from "react"
 import "./app.scss"
 import airPlane from "./assets/airPlane.jpg"
 import Sorting from "./components/Sorting/Sorting"
@@ -7,40 +8,60 @@ import useGetTickets from "./hooks/useGetTickets"
 
 export default function App() {
     const { tickets, loading, error } = useGetTickets()
+    const [sorting, setSorting] = useState("lowPrice")
+    const [transfer, setTransfer] = useState("all")
+
+    const filterData = useMemo(() => {
+        return [...tickets].filter((ticket) => {
+            switch (transfer) {
+                case "all":
+                    return true
+                case "without":
+                    return ticket.transfers == 0
+            }
+        })
+    }, [tickets, transfer])
+
+    const sortedData = useMemo(() => {
+        return [...filterData].sort((a, b) => {
+            switch (sorting) {
+                case "lowPrice":
+                    return a.priceInt - b.priceInt
+                case "fast":
+                    return a.flightInfo.durationInt - b.flightInfo.durationInt
+            }
+        })
+    }, [filterData, sorting])
 
     return (
         <>
-            <div className="image">
-                <img className="image__content" src={airPlane} alt="" />
-            </div>
-            <div className="content">
-                <div className="content__leftPanel">
-                    <Transfer
-                        onClick={(tabName) =>
-                            console.log(
-                                "В блоке Transfer выбрана вкладка: " + tabName
-                            )
-                        }
-                    />
-                </div>
-                <div className="content__rightPanel">
-                    <Sorting
-                        onClick={(tabName) =>
-                            console.log(
-                                "В блоке Sorting выбрана вкладка: " + tabName
-                            )
-                        }
-                    />
-                    {loading && <span>Загрузка...</span>}
-                    {!loading &&
-                        tickets.map((ticket) => (
-                            <TicketCard
-                                key={ticket.id}
-                                ticketData={ticket}
-                            ></TicketCard>
-                        ))}
-                </div>
-            </div>
+            {loading && <span>Загрузка...</span>}
+            {!loading && (
+                <>
+                    <div className="image">
+                        <img className="image__content" src={airPlane} alt="" />
+                    </div>
+                    <div className="content">
+                        <div className="content__leftPanel">
+                            <Transfer
+                                onClick={(tabName) => setTransfer(tabName)}
+                                ticketsData={tickets}
+                            />
+                        </div>
+                        <div className="content__rightPanel">
+                            <Sorting
+                                onClick={(tabName) => setSorting(tabName)}
+                            />
+                            {sortedData.map((ticket) => (
+                                <TicketCard
+                                    key={ticket.id}
+                                    ticketData={ticket}
+                                ></TicketCard>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     )
 }
