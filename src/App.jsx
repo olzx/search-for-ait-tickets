ae9e9573b490e9302a185526293418959c3c8183
Олежка <ol2az@yandex.ru>
2024-12-15 13:38:59 +0700

    added component for display more flight card info

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,24 @@ import useGetTickets from "./hooks/useGetTickets"
 import filterDataFunc from "./utils/filterDataFunc"
 import sortedDataFunc from "./utils/sortedDataFunc"
 import Preloader from "./components/Preloader/Preloader"
+import ShowInfoTickedCard from "./components/ShowInfoTickedCard/ShowInfoTickedCard"
 
 export default function App() {
     const { tickets, loading, error } = useGetTickets()
     const [sorting, setSorting] = useState("lowPrice")
     const [transfer, setTransfer] = useState("all")
     const [selectTicketCard, setSelectTicketCard] = useState(false)
+    const [isOpenModal, setIsOpenModal] = useState(false)
 
     console.log(selectTicketCard)
 
     const filterData = filterDataFunc(tickets, transfer) // фильтрация по кол-ву пересадок
     const sortedData = sortedDataFunc(filterData, sorting) // сортирует уже отфильтрованый массив по самому дешевому или самому быстрому
+
+    function clickSelectTicketCard(ticketData) {
+        setSelectTicketCard(ticketData)
+        setIsOpenModal((prev) => !prev)
+    }
 
     return (
         <>
@@ -43,11 +50,20 @@ export default function App() {
                                 <TicketCard
                                     key={ticket.id}
                                     ticketData={ticket}
-                                    onClick={() => setSelectTicketCard(ticket)}
+                                    onClick={() =>
+                                        clickSelectTicketCard(ticket)
+                                    }
                                 ></TicketCard>
                             ))}
                         </div>
                     </div>
+                    {selectTicketCard && (
+                        <ShowInfoTickedCard
+                            isOpenModal={isOpenModal}
+                            onCloseModal={() => setIsOpenModal(false)}
+                            selectTicketCard={selectTicketCard}
+                        ></ShowInfoTickedCard>
+                    )}
                 </>
             )}
         </>
---
+++ src/components/ShowInfoTickedCard/ShowInfoTickedCard.jsx
@@ -0,0 +1,76 @@
+import "./ShowInfoTickedCard.scss"
+import Modal from "../Modal/Modal"
+import Button from "../Button/Button"
+
+export default function ShowInfoTickedCard({
+    isOpenModal,
+    onCloseModal,
+    selectTicketCard,
+}) {
+    return (
+        <Modal isOpen={isOpenModal}>
+            <h3>Информация о перелете</h3>
+            <hr />
+            <div className="flightInfo">
+                <div className="flightInfo__left">
+                    <div className="flightInfo__time">
+                        {selectTicketCard.flightInfo.departureTime[0]}
+                    </div>
+                    <div className="flightInfo__city">
+                        {selectTicketCard.flightInfo.origin}
+                    </div>
+                    <div className="flightInfo__date">
+                        {selectTicketCard.flightInfo.departureTime[1]}
+                    </div>
+                </div>
+                <div className="flightInfo__middle">
+                    <div className="flightInfo__duration">
+                        В пути: {selectTicketCard.flightInfo.duration}
+                    </div>
+                </div>
+                <div className="flightInfo__right">
+                    <div className="flightInfo__time">
+                        {selectTicketCard.flightInfo.arrivalTime[0]}
+                    </div>
+                    <div className="flightInfo__city">
+                        {selectTicketCard.flightInfo.destination}
+                    </div>
+                    <div className="flightInfo__date">
+                        {selectTicketCard.flightInfo.arrivalTime[1]}
+                    </div>
+                </div>
+            </div>
+            <ul>
+                <li>
+                    Авиакомпания: <b>{selectTicketCard.airline}</b>
+                </li>
+                <li>
+                    Стоимость: <b>{selectTicketCard.price}</b>
+                </li>
+                <li>
+                    Класс перелета: <b>{selectTicketCard.class}</b>
+                </li>
+                <li>
+                    Наличие багажа:{" "}
+                    <b>{selectTicketCard.baggage ? "Да" : "Нет"}</b>
+                </li>
+                <li>
+                    Города для пересадки:{" "}
+                    {selectTicketCard.transfers ? (
+                        <ul>
+                            {selectTicketCard.transferLocations.map(
+                                (transfer) => (
+                                    <li key={transfer}>{transfer}</li>
+                                )
+                            )}
+                        </ul>
+                    ) : (
+                        <span>Нет</span>
+                    )}
+                </li>
+            </ul>
+            <hr />
+            <Button onClick={() => onCloseModal()}>Закрыть</Button>
+        </Modal>
+    )
+}
